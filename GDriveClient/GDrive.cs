ed0443d692b8d390f9ced55f55ef247fb2ca3fae
Sunil <sonuame@aol.com>
2017-07-31 19:24:08 +0530

    Add project files.

---
+++ GDriveClient/GDrive.cs
@@ -0,0 +1,100 @@
+﻿using Google.Apis.Drive.v3;
+using Google.Apis.Sheets.v4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using System.Security.Cryptography;
+using Google.Apis.Drive.v3.Data;
+
+namespace GDriveClient
+{
+    public enum ServiceType
+    {
+        Drive,
+        Spreadsheet
+    }
+    public class GDrive
+    {
+        public string APIKey { get; set; }
+        public DriveService driveService { get; set; }
+        public SheetsService sheetService { get; set; }
+        public GDrive(ServiceType service)
+        {
+            var certificate = new X509Certificate2(Directory.GetCurrentDirectory() + "\\ConnectJS-4f5b836be640.p12", "notasecret", X509KeyStorageFlags.Exportable);
+            if (service == ServiceType.Drive)
+            {
+                string[] scopes = new string[] { DriveService.Scope.Drive };
+
+                var credential = new ServiceAccountCredential(new ServiceAccountCredential.Initializer("connectjs-admin@connectjs-175408.iam.gserviceaccount.com")
+                {
+                    Scopes = scopes
+                }.FromCertificate(certificate));
+
+
+
+                driveService = new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "ConnectJS",
+                    ApiKey = "AIzaSyAseLqm2MH01eTiFbrEE_9WeY5lkl7TcDQ"
+                });
+
+            }
+
+            if (service == ServiceType.Spreadsheet)
+            {
+                string[] scopes = new string[] { SheetsService.Scope.Spreadsheets, SheetsService.Scope.Drive };
+
+                var credential = new ServiceAccountCredential(new ServiceAccountCredential.Initializer("connectjs-admin@connectjs-175408.iam.gserviceaccount.com")
+                {
+                    Scopes = scopes
+                }.FromCertificate(certificate));
+
+                sheetService = new SheetsService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "ConnectJS",
+                    ApiKey = "AIzaSyAseLqm2MH01eTiFbrEE_9WeY5lkl7TcDQ"
+                });
+
+
+
+
+
+            }
+
+        }
+        public GDrive()
+        {
+
+        }
+
+        public List<Revision> GetRevisions(string fileID)
+        {
+            if (driveService != null)
+            {
+                var RevisionRequest = driveService.Revisions.List(fileID);
+                RevisionRequest.Fields = "*";
+                RevisionList changes = RevisionRequest.Execute();
+                return changes.Revisions.OrderByDescending(m=>int.Parse(m.Id)).ToList();
+            }
+            return new List<Revision>();
+        }
+
+        public Revision GetLatestRevision(string fileID)
+        {
+            if (driveService != null)
+            {
+                return GetRevisions(fileID).FirstOrDefault();
+            }
+            return null;
+        }
+
+    }
+}
---
+++ GDriveVersionTracker/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GDriveClient;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+
+namespace GDriveVersionTracker
+{
+    class Program
+    {
+        public static int LatestRevID { get; set; }
+        static string fileID = "1XrhDKZtHWMkPmPI8YsaQVsUcdAWpraozHhNB_39dnwM";
+        static GDrive g;
+        static List<Revision> Revisions;
+        static void Main(string[] args)
+        {
+            g = new GDrive(ServiceType.Drive);
+            Revisions = new List<Revision>();
+            GetRevisions();
+            RevisionPooling();
+            Console.ReadKey();
+        }
+
+        public static async void GetRevisions()
+        {
+            await Task.Run(() =>
+            {
+                List<Revision> Revisions = g.GetRevisions(fileID);
+                foreach (Revision item in Revisions)
+                {
+                    LatestRevID = LatestRevID == 0 ? int.Parse(item.Id) : LatestRevID;
+                    Console.WriteLine(item.ModifiedTime + " - " + item.LastModifyingUser.DisplayName);
+                }
+                System.Threading.Thread.Sleep(2000);
+            });
+        }
+
+        public static async void RevisionPooling()
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    Revision r = g.GetLatestRevision(fileID);
+                    if(r!=null && int.Parse(r.Id) > LatestRevID)
+                    {
+                        LatestRevID = int.Parse(r.Id);
+                        Revisions.Insert(0, r);
+                        Console.Clear();
+
+                        foreach (Revision item in Revisions)
+                            Console.WriteLine(item.ModifiedTime + " - " + item.LastModifyingUser.DisplayName);
+                    }
+                    System.Threading.Thread.Sleep(2000);
+                }
+            });
+        }
+    }
+}
